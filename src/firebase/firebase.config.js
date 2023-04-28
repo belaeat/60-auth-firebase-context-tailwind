577b1f9f4f3b6b05cae5e72d0f907e1cf56b3a52
belaeat <omyhossain@gmail.com>
2023-04-28 22:59:42 +0600

    auth master learning

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_zwMg7yQXqGj5wnu54S-QO6a9e9vPTe8",
+  authDomain: "fir-context-tailwind-f8562.firebaseapp.com",
+  projectId: "fir-context-tailwind-f8562",
+  storageBucket: "fir-context-tailwind-f8562.appspot.com",
+  messagingSenderId: "827132020186",
+  appId: "1:827132020186:web:811e77d8d193660be02ce3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
